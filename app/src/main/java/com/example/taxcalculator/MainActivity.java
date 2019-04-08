26e2ec192cb67338412d8c2497de1175265395d1
shiva <shivamaharjan1234567890@gmail.com>
2019-04-08 15:49:09 +0545

    Complete

--- app/src/main/java/com/example/taxcalculator/MainActivity.java
+++ app/src/main/java/com/example/taxcalculator/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private TextView tvResult;
+    private TextView tvResult, tvPrevAmt, tvDeduct;
     private Button btnCalculate;
     private EditText etAmount;
 
@@ -21,13 +21,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tvResult = findViewById(R.id.tvResult);
         btnCalculate = findViewById(R.id.btnCalculate);
         etAmount = findViewById(R.id.etTax);
+        tvPrevAmt = findViewById(R.id.tvAmount);
+        tvDeduct = findViewById(R.id.tvDeduction);
 
         btnCalculate.setOnClickListener(this);
     }
 
-    public boolean validate(){
+    public boolean validate() {
         boolean flag = true;
-        if(TextUtils.isEmpty(etAmount.getText().toString())){
+        if (TextUtils.isEmpty(etAmount.getText().toString())) {
             etAmount.setError("Please Enter Your Amount");
             etAmount.requestFocus();
             flag = false;
@@ -35,19 +37,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         return flag;
     }
+
     @Override
     public void onClick(View v) {
-        double amount = Double.parseDouble(etAmount.getText().toString());
-        if(validate()){
-            if(v.getId() == R.id.btnCalculate){
+        if (validate()) {
+            double amount = Double.parseDouble(etAmount.getText().toString());
+            if (v.getId() == R.id.btnCalculate) {
 
                 TaxCalculation tx = new TaxCalculation();
                 tx.setAmount(amount);
+                tvDeduct.setText(Float.toString(tx.taxAmount(amount)));
+                tvPrevAmt.setText(Double.toString(amount));
                 tvResult.setText(Double.toString(tx.getAmount()));
-
-
+                etAmount.setText("");
             }
         }
-
     }
 }
--- app/src/main/java/com/example/taxcalculator/TaxCalculation.java
+++ app/src/main/java/com/example/taxcalculator/TaxCalculation.java
@@ -6,18 +6,23 @@ public class TaxCalculation {
         if(amount>350000){
             double threeLakh = amount - 350000;
             amount = 350000;
-            amt += (threeLakh+(0.25*(threeLakh)));
+            amt += (threeLakh-(0.25*(threeLakh)));
         }
         if(amount<=350000 && amount >=200000){
             double fifteenPer = amount - 200000;
             amount = 200000;
-            amt += (fifteenPer+(0.15*(fifteenPer)));
+            amt += (fifteenPer-(0.15*(fifteenPer)));
         }
         if(amount<=200000 && amount>=1){
-            amt += (amount+(0.01*(amount)));
+            amt += (amount-(0.01*(amount)));
         }
     }
     public double getAmount(){
+        return amt;
+    }
+
+    public float taxAmount(double amount){
+        float amt = (float) (amount - getAmount());
         return amt;
     }
 }
